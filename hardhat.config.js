57f750ddaf882b46876f1e65c8f3067c74f56101
Said <said-ibragimov-02@mail.ru>
2022-12-30 20:08:13 +0300

    change hardhat.config.js and delete Lock.sol

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
-require("@nomicfoundation/hardhat-toolbox");
+import "@nomicfoundation/hardhat-toolbox";
+require('dotenv').config()
+
+
+const CHAIN_IDS = {
+  hardhat: 31337,
+};
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.17",
+export const defaultNetwork = "hardhat";
+export const networks = {
+  hardhat: {
+    chainId: CHAIN_IDS.hardhat,
+    forking: {
+      url: "https://eth-mainnet.g.alchemy.com/v2/lhprCKFZGUDhc7nj9SeYqA7myg-u-TnY",
+    },
+  },
 };
+export const solidity = "0.8.17";
